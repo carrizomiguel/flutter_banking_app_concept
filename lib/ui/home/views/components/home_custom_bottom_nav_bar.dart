8c210a0a39c5a1fa7ecdf81c843e61060f8298ae
carrizomiguel <carrizo2000miguel@gmail.com>
2022-02-02 13:52:50 -0500

    improving bottom nav bar

--- lib/ui/home/views/components/home_custom_bottom_nav_bar.dart
+++ lib/ui/home/views/components/home_custom_bottom_nav_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomeCustomBottomNavBar extends StatefulWidget {
-  const HomeCustomBottomNavBar({ Key? key }) : super(key: key);
+  const HomeCustomBottomNavBar({Key? key}) : super(key: key);
 
   @override
   State<HomeCustomBottomNavBar> createState() => _HomeCustomBottomNavBarState();
@@ -46,32 +46,38 @@ class _HomeCustomBottomNavBarState extends State<HomeCustomBottomNavBar> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              ..._items.map((key, value) => MapEntry(
-                key, Column(
-                  children: [
-                    IconButton(
-                      onPressed: () => _changeView(key),
-                      icon: Icon(value['icon']),
-                      color: _viewIs == key
-                          ? Colors.white
-                          : Colors.grey.shade600,
-                    ),
-                    const Spacer(),
-                    AnimatedContainer(
-                      height: 5,
-                      width: _viewIs == key ? 40 : 0,
-                      duration: const Duration(milliseconds: 150),
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(10),
-                          topRight: Radius.circular(10),
-                        )
-                      ),
-                    ),
-                  ],
-                ),
-              )).values.toList(),
+              ..._items
+                  .map((key, value) => MapEntry(
+                        key,
+                        Column(
+                          children: [
+                            IconButton(
+                              onPressed: () => _changeView(key),
+                              icon: Icon(
+                                value['icon'],
+                                size: 28,
+                              ),
+                              color: _viewIs == key
+                                  ? const Color(0xFFEBDCDA)
+                                  : Colors.grey.shade600,
+                            ),
+                            const Spacer(),
+                            AnimatedContainer(
+                              height: 5,
+                              width: _viewIs == key ? 35 : 0,
+                              duration: const Duration(milliseconds: 150),
+                              decoration: const BoxDecoration(
+                                  color: Color(0xFFEBDCDA),
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(10),
+                                    topRight: Radius.circular(10),
+                                  )),
+                            ),
+                          ],
+                        ),
+                      ))
+                  .values
+                  .toList(),
             ],
           ),
         ),
