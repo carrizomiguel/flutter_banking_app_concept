e7e6e524ae6712d2a99ab97f446c8412283918ed
carrizomiguel <carrizo2000miguel@gmail.com>
2022-02-02 14:30:10 -0500

    created card element file

---
+++ lib/ui/home/views/components/cards/card_element.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CardElement extends StatelessWidget {
+  const CardElement({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
