6c8d08ba3a1d5b981d86be8e91149731e5422937
carrizomiguel <carrizo2000miguel@gmail.com>
2022-02-06 00:02:50 -0500

    cards view finished

--- lib/ui/home/views/components/cards/card_element.dart
+++ lib/ui/home/views/components/cards/card_element.dart
@@ -1,13 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CardElement extends StatelessWidget {
-  const CardElement({Key? key}) : super(key: key);
+  const CardElement({
+    Key? key,
+    required this.card,
+  }) : super(key: key);
+
+  final Map<String, dynamic> card;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(20),
-      child: const Text('Hola Mundo!'),
+      padding: const EdgeInsets.all(40),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              SvgPicture.asset(
+                card['bank'],
+                height: 60,
+                color: Colors.black87,
+              ),
+              const Spacer(),
+              Text(
+                card['date'],
+                style: const TextStyle(
+                  color: Colors.black87,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+          Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    card['type'],
+                    style: const TextStyle(
+                      color: Colors.black87,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Text(
+                    card['number'],
+                    style: const TextStyle(
+                      color: Colors.black87,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Container(
+                padding: const EdgeInsets.all(17),
+                child: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+                decoration: const BoxDecoration(
+                  color: Color(0xFFABBBCC),
+                  shape: BoxShape.circle,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
       decoration: BoxDecoration(
         gradient: const LinearGradient(
             begin: Alignment.topLeft,
@@ -16,7 +82,7 @@ class CardElement extends StatelessWidget {
               Color(0xFFF8D8CB),
               Color(0xFF8CAACD),
             ]),
-        borderRadius: BorderRadius.circular(25),
+        borderRadius: BorderRadius.circular(45),
         boxShadow: const [
           BoxShadow(
             color: Colors.black38,
--- lib/ui/home/views/components/cards/card_swiper.dart
+++ lib/ui/home/views/components/cards/card_swiper.dart
@@ -3,17 +3,40 @@ import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
-  const CardSwiper({ Key? key }) : super(key: key);
+  const CardSwiper({Key? key}) : super(key: key);
+
+  final List<Map<String, dynamic>> _cards = const [
+    {
+      'bank': 'assets/logos/visa.svg',
+      'type': 'Credit Card',
+      'number': '**** **** 2499 4194',
+      'date': '08/23',
+    },
+    {
+      'bank': 'assets/logos/mastercard.svg',
+      'type': 'Debit Card',
+      'number': '**** **** 5923 8429',
+      'date': '02/25',
+    },
+    {
+      'bank': 'assets/logos/visa.svg',
+      'type': 'Credit Card',
+      'number': '**** **** 3289 7502',
+      'date': '08/22',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Swiper(
       itemCount: 3,
       itemBuilder: (context, index) {
-        return const CardElement();
+        return CardElement(
+          card: _cards[index],
+        );
       },
       itemWidth: double.infinity,
-      itemHeight: 250,
+      itemHeight: 270,
       layout: SwiperLayout.TINDER,
     );
   }
